c69e580532418b00972e3f0ab23b6e717b393e48
Carlos <clopez.km@gmail.com>
2020-11-11 23:41:12 -0500

    test

--- src/main/java/com/co/academia/security/AuthenticationManager.java
+++ src/main/java/com/co/academia/security/AuthenticationManager.java
@@ -31,7 +31,6 @@ public class AuthenticationManager implements ReactiveAuthenticationManager{
 			usuario = null;
 		}
 		
-		
 		if (usuario != null && jwtUtil.validateToken(token)) {
 			Claims claims = jwtUtil.getAllClaimsFromToken(token);
 			
